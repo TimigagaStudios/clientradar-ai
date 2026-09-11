b561b745e03db95043961b87e1b324d6e83340a9
TimigagaStudios <timigaga.official@gmail.com>
2026-09-11 10:18:17 +0100

    Add files via upload

--- src/pages/DemoPreview.tsx
+++ src/pages/DemoPreview.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ArrowLeft, CheckCircle2, Mail, MapPin, Phone, Sparkles } from 'lucide-react';
 import { Link } from 'react-router-dom';
